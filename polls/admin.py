e03322ccb9f25f12c4dcfb30ad86883b7978a153
Your Name <samya@gmail.com>
2024-01-28 12:35:06 +0530

    first commit

---
+++ polls/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from .models import Question, Choice
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_name"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = [
+        "question_name",
+        "pub_date",
+        "was_published_recently",
+        "no_of_choices",
+    ]
+    list_filter = ["pub_date"]
+    search_fields = ["question_name"]
+
+    def no_of_choices(self, question):
+        return question.choice_set.count()
+
+
+admin.site.register(Question, QuestionAdmin)
